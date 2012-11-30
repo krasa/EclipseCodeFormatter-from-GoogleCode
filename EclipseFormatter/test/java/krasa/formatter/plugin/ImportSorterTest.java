4efb12dfc8c87a28c304864d5c6f37373de314d4
vojta.krasa@gmail.com <vojta.krasa@gmail.com@e27bc898-82c9-97d5-db3a-ba6cebccf294>
2012-11-30 21:59:43 +0000

    3.1 fixed redundant new line at imports

--- EclipseFormatter/test/java/krasa/formatter/plugin/ImportSorterTest.java
+++ EclipseFormatter/test/java/krasa/formatter/plugin/ImportSorterTest.java
@@ -18,8 +18,8 @@ public class ImportSorterTest {
 	public static final List<String> DEFAULT_ORDER = Arrays.asList("java", "javax", "org", "com");
 
 	@Test
-	public void testName() throws Exception {
-		String EXPECTED = "import java.util.HashMap;\n" + "import java.util.Map;\n" + "\n"
+	public void test() throws Exception {
+		String expected = "import java.util.HashMap;\n" + "import java.util.Map;\n" + "\n"
 				+ "import org.jingle.mocquer.MockControl;\n" + N + "import sun.security.action.GetLongAction;\n"
 				+ "import tmplatform.authorisation.ApiClientLink;\n"
 				+ "import tmplatform.comms.common.caaa.EvasLdapInterfaceProfileWrapper;\n"
@@ -34,12 +34,12 @@ public class ImportSorterTest {
 		List<String> importsOrder = Arrays.asList("java", "javax", "org", "com");
 		ImportSorter importSorter = new ImportSorter(importsOrder);
 		List<String> strings = importSorter.sortByEclipseStandard(imports);
-		printAndAssert(EXPECTED, strings);
+		printAndAssert(expected, strings);
 	}
 
 	@Test
-	public void testName2() throws Exception {
-		String EXPECTED = "import static com.MyUtil.kuk;\n" + "import static org.junit.Assert.assertNotNull;\n"
+	public void test2() throws Exception {
+		String expected = "import static com.MyUtil.kuk;\n" + "import static org.junit.Assert.assertNotNull;\n"
 				+ "import static tmutil.StringUtil.replaceText;\n\n" + "import java.util.HashMap;\n"
 				+ "import java.util.Map;\n" + "\n" + "import org.jingle.mocquer.MockControl;\n" + N
 				+ "import sun.security.action.GetLongAction;\n" + "import tmplatform.authorisation.ApiClientLink;\n"
@@ -56,12 +56,12 @@ public class ImportSorterTest {
 		List<String> importsOrder = Arrays.asList("java", "javax", "org", "com");
 		ImportSorter importSorter = new ImportSorter(importsOrder);
 		List<String> strings = importSorter.sortByEclipseStandard(StringUtils.trimImport(imports));
-		printAndAssert(EXPECTED, strings);
+		printAndAssert(expected, strings);
 	}
 
 	@Test
-	public void testName3() throws Exception {
-		String EXPECTED = "import static com.MyUtil.kuk;\n" + "import static org.junit.Assert.assertNotNull;\n"
+	public void test3() throws Exception {
+		String expected = "import static com.MyUtil.kuk;\n" + "import static org.junit.Assert.assertNotNull;\n"
 				+ "import static tmutil.StringUtil.replaceText;\n\n" + "import java.util.ArrayList;\n" + "\n"
 				+ "import org.w3c.dom.DOMConfiguration;\n" + "import org.w3c.dom.DOMException;\n"
 				+ "import org.w3c.dom.Document;\n" + "import org.w3c.dom.Node;\n"
@@ -77,62 +77,42 @@ public class ImportSorterTest {
 				+ "import org.w3c.dom.ls.LSParserFilter;\n" + "import org.xml.sax.InputSource;\n" + N
 				+ "import java.util.ArrayList;";
 
-		List<String> importsOrder = Arrays.asList("java", "javax", "org", "com");
-		ImportSorter importSorter = new ImportSorter(importsOrder);
+		ImportSorter importSorter = new ImportSorter(DEFAULT_ORDER);
 
 		List<String> imports1 = StringUtils.trimImport(imports);
 		System.err.println(Arrays.toString(imports1.toArray()));
 		List<String> strings = importSorter.sortByEclipseStandard(imports1);
-		printAndAssert(EXPECTED, strings);
+		printAndAssert(expected, strings);
 	}
 
 	@Test
-	public void testName4() throws Exception {
-		String EXPECTED = "import java.util.Arrays;\n" +
-				"\n" +
-				"import models.Deployment;\n" +
-				"import play.jobs.Job;\n" +
-				"import play.mvc.Before;\n" +
-				"import controllers.deadbolt.Restrict;\n";
+	public void test4() throws Exception {
+		String expected = "import java.util.Arrays;\n" + "\n" + "import models.Deployment;\n"
+				+ "import play.jobs.Job;\n" + "import play.mvc.Before;\n" + "import controllers.deadbolt.Restrict;\n";
 
-		String imports = "import controllers.deadbolt.Restrict;\n" +
-				"import java.util.Arrays;\n" +
-				"import play.mvc.Before;\n" +
-				"import models.Deployment;\n" +
-				"import play.jobs.Job;\n";
+		String imports = "import controllers.deadbolt.Restrict;\n" + "import java.util.Arrays;\n"
+				+ "import play.mvc.Before;\n" + "import models.Deployment;\n" + "import play.jobs.Job;\n";
 
-		List<String> importsOrder = DEFAULT_ORDER;
-		ImportSorter importSorter = new ImportSorter(importsOrder);
+		ImportSorter importSorter = new ImportSorter(DEFAULT_ORDER);
 
 		List<String> imports1 = StringUtils.trimImport(imports);
 		System.err.println(Arrays.toString(imports1.toArray()));
 		List<String> strings = importSorter.sortByEclipseStandard(imports1);
-		printAndAssert(EXPECTED, strings);
+		printAndAssert(expected, strings);
 	}
 
 	@Test
-	public void testName5() throws Exception {
-		String EXPECTED =
-				"import static java.lang.Integer.numberOfLeadingZeros;\n" +
-						"import static java.lang.Integer.valueOf;\n" +
-						"\n" +
-						"import java.sql.Date;\n" +
-						"import java.util.List;\n" +
-						"import java.util.Map;\n" +
-						"import javax.xml.crypto.dsig.spec.HMACParameterSpec;\n" +
-						"import org.w3c.dom.Text;\n" +
-						"import org.w3c.dom.stylesheets.StyleSheetList;\n";
+	public void test5() throws Exception {
+		String expected = "import static java.lang.Integer.numberOfLeadingZeros;\n"
+				+ "import static java.lang.Integer.valueOf;\n" + "\n" + "import java.sql.Date;\n"
+				+ "import java.util.List;\n" + "import java.util.Map;\n"
+				+ "import javax.xml.crypto.dsig.spec.HMACParameterSpec;\n" + "import org.w3c.dom.Text;\n"
+				+ "import org.w3c.dom.stylesheets.StyleSheetList;\n";
 
-		String imports =
-				"import javax.xml.crypto.dsig.spec.HMACParameterSpec;\n" +
-						"import org.w3c.dom.Text;\n" +
-						"import java.util.List;\n" +
-						"import static java.lang.Integer.numberOfLeadingZeros;\n" +
-						"import java.sql.Date;\n" +
-						"\n" +
-						"import static java.lang.Integer.valueOf;\n" +
-						"import java.util.Map;\n" +
-						"import org.w3c.dom.stylesheets.StyleSheetList;\n";
+		String imports = "import javax.xml.crypto.dsig.spec.HMACParameterSpec;\n" + "import org.w3c.dom.Text;\n"
+				+ "import java.util.List;\n" + "import static java.lang.Integer.numberOfLeadingZeros;\n"
+				+ "import java.sql.Date;\n" + "\n" + "import static java.lang.Integer.valueOf;\n"
+				+ "import java.util.Map;\n" + "import org.w3c.dom.stylesheets.StyleSheetList;\n";
 
 		List<String> importsOrder = Collections.emptyList();
 		ImportSorter importSorter = new ImportSorter(importsOrder);
@@ -140,62 +120,68 @@ public class ImportSorterTest {
 		List<String> imports1 = StringUtils.trimImport(imports);
 		System.err.println(Arrays.toString(imports1.toArray()));
 		List<String> strings = importSorter.sortByEclipseStandard(imports1);
-		printAndAssert(EXPECTED, strings);
+		printAndAssert(expected, strings);
 	}
 
 	@Test
-	public void testName6() throws Exception {
-		String document =
-                "package jobs;\n" +
-                        "\n" +
-                        "import models.Album;\n" +
-                        "import models.Picture;\n" +
-                        "import org.apache.commons.lang.StringUtils;\n" +
-                        "import org.apache.http.HttpEntity;\n" +
-                        "import org.apache.http.HttpResponse;\n" +
-                        "import org.apache.http.HttpStatus;\n" +
-                        "import org.apache.http.client.methods.HttpGet;\n" +
-                        "import org.apache.http.impl.client.DefaultHttpClient;\n" +
-                        "import org.apache.http.params.CoreConnectionPNames;\n" +
-                        "import org.apache.http.util.EntityUtils;\n" +
-                        "import org.jsoup.Jsoup;\n" +
-                        "import org.jsoup.nodes.Document;\n" +
-                        "import org.jsoup.nodes.Element;\n" +
-                        "import org.jsoup.select.Elements;\n" +
-                        "import play.Logger;\n" +
-                        "import play.db.jpa.JPA;\n" +
-                        "import play.jobs.Job;\n" +
-                        "import play.libs.Codec;\n" +
-                        "import play.mvc.Router;\n" +
-                        "import play.vfs.VirtualFile;\n" +
-                        "import utils.BaseX;\n" +
-                        "import utils.UpYunUtils;\n" +
-                        "\n" +
-                        "import java.io.File;\n" +
-                        "import java.io.FileOutputStream;\n" +
-                        "import java.io.IOException;\n" +
-                        "import java.io.InputStream;\n" +
-                        "import java.math.BigInteger;\n" +
-                        "\n" +
-                        "/**\n" +
-                        " * User: divxer Date: 12-6-4 Time: 上午12:17\n" +
-                        " */\n" +
-                        "// @Every(\"7h\")\n" +
-                        "// @OnApplicationStart(async=true)\n" +
-                        "public class Picture4493Crawler extends Job {\n\n}";
+	public void test6() throws Exception {
+		String document = "package jobs;\n" + "\n" + "import models.Album;\n" + "import models.Picture;\n"
+				+ "import org.apache.commons.lang.StringUtils;\n" + "import org.apache.http.HttpEntity;\n"
+				+ "import org.apache.http.HttpResponse;\n" + "import org.apache.http.HttpStatus;\n"
+				+ "import org.apache.http.client.methods.HttpGet;\n"
+				+ "import org.apache.http.impl.client.DefaultHttpClient;\n"
+				+ "import org.apache.http.params.CoreConnectionPNames;\n"
+				+ "import org.apache.http.util.EntityUtils;\n" + "import org.jsoup.Jsoup;\n"
+				+ "import org.jsoup.nodes.Document;\n" + "import org.jsoup.nodes.Element;\n"
+				+ "import org.jsoup.select.Elements;\n" + "import play.Logger;\n" + "import play.db.jpa.JPA;\n"
+				+ "import play.jobs.Job;\n" + "import play.libs.Codec;\n" + "import play.mvc.Router;\n"
+				+ "import play.vfs.VirtualFile;\n" + "import utils.BaseX;\n" + "import utils.UpYunUtils;\n" + "\n"
+				+ "import java.io.File;\n" + "import java.io.FileOutputStream;\n" + "import java.io.IOException;\n"
+				+ "import java.io.InputStream;\n" + "import java.math.BigInteger;\n" + "\n" + "/**\n"
+				+ " * User: divxer Date: 12-6-4 Time: 上午12:17\n" + " */\n" + "// @Every(\"7h\")\n"
+				+ "// @OnApplicationStart(async=true)\n" + "public class Picture4493Crawler extends Job {\n\n}";
 
-        List<String> importsOrder = Collections.emptyList();
+		List<String> importsOrder = Collections.emptyList();
 		ImportSorter importSorter = new ImportSorter(importsOrder);
-        MockDocument document1 = new MockDocument(document);
-        importSorter.sortImports(document1);
-        System.err.println(document1.getText());
-    }
+		MockDocument document1 = new MockDocument(document);
+		importSorter.sortImports(document1);
+		System.err.println(document1.getText());
+	}
 
-	private void printAndAssert(String EXPECTED, List<String> strings) {
+	@Test
+	public void test7() throws Exception {
+		String imports = "import java.util.Calendar;";
+
+		String expected = "import java.util.Calendar;\n";
+
+		List<String> importsOrder = Arrays.asList("com", "java", "javax", "org");
+		ImportSorter importSorter = new ImportSorter(importsOrder);
+
+		List<String> imports1 = StringUtils.trimImport(imports);
+		System.err.println(Arrays.toString(imports1.toArray()));
+		List<String> strings = importSorter.sortByEclipseStandard(imports1);
+		printAndAssert(expected, strings);
+	}
+
+	@Test
+	public void test8() throws Exception {
+		String imports = "";
+
+		String expected = "";
+
+		ImportSorter importSorter = new ImportSorter(DEFAULT_ORDER);
+
+		List<String> imports1 = StringUtils.trimImport(imports);
+		System.err.println(Arrays.toString(imports1.toArray()));
+		List<String> strings = importSorter.sortByEclipseStandard(imports1);
+		printAndAssert(expected, strings);
+	}
+
+	private void printAndAssert(String expected, List<String> strings) {
 		StringBuilder stringBuilder = print(strings);
-		System.out.println("-----EXPECTED------");
-		System.out.println(EXPECTED);
-		Assert.assertEquals(EXPECTED, stringBuilder.toString());
+		System.out.println("-----expected------");
+		System.out.println(expected);
+		Assert.assertEquals(expected, stringBuilder.toString());
 		System.out.println("-----------------");
 
 	}
